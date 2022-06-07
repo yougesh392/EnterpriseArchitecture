7a0fedbec85fa0b7a37899f76b2a8cedd777aaa6
Yougesh Kumar <ypinjani@gmail.com>
2022-06-06 22:19:48 -0500

    Assignment_12 started remaing 9 and 10 should be added

---
+++ Assignment_12/W3D1-MVC/src/main/java/edu/mum/cs544/MyWebAppInitializer.java
@@ -0,0 +1,30 @@
+package edu.mum.cs544;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MyWebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+        // Create the Spring 'root' application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(Config.class);
+
+        // Manage the lifecycle of the root application context
+        container.addListener(new ContextLoaderListener(rootContext));
+        
+		ServletRegistration.Dynamic appServlet = container.addServlet("mvc",
+				new DispatcherServlet(new GenericWebApplicationContext()));
+		appServlet.setLoadOnStartup(1);
+		appServlet.addMapping("/");
+
+    }
+}
